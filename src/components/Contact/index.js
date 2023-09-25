f2f43fd4e4549aec5101d75a6c2b945eb59364a2
AlexJohnAnderson <alex.anderson095439@gmail.com>
2023-09-25 09:26:17 -0500

    Added contact info

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,22 +10,12 @@ const Contact = () => {
       <div className="container contact-page">
         <div className="text-zone">
           <h1>
-            <AnimatedLetters
-            
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-              idx={15}
-            />
+            Contact me   
+          </h1>
+          <h1>
+          Alex Anderson | Lawrence, Kansas | (620)-803-2355 | alex.anderson095439@gmail.com
           </h1>
           </div>
-        <div className="info-map">
-          Alex Anderson,
-          <br />
-          Lawrence, Kansas,
-          <br />
-          (620)-803-2355
-          <br />
-          <span>alex.anderson095439@gmail.com</span>
-        </div>
       </div>
       <Loader type="pacman" />
     </>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import myImage from '../../assets/images/resume.jpg';
 import "./index.scss";
 
-function Portfolio() {
-    return (
-      <div className="image-container">
-        <img src={myImage} alt="Professional" className="full-width-image" />
+function ResizableImage() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Update the windowWidth state when the window is resized
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="image-container">
+      <div className="image-wrapper">
+        <img
+          src={myImage}
+          alt="Professional"
+          style={{ maxWidth: '100%', height: 'auto', width: '100%' }}
+        />
       </div>
-    );
-  }
-  
-  export default Portfolio;
+    </div>
+  );
+}
+
+export default ResizableImage;
